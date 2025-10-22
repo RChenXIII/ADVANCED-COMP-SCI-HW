ab0a7705c5c7d0863c3fde1ec3ca9ef1b56b812c
RChenXIII <ryanchen2231@gmail.com>
2025-10-22 13:31:09 -0700

    Unit 1 exercises
    DId unit 1 exercises. Not finished yet

---
+++ Unit Lessons/Unit 1/U1Exercises/Unit1ExercisesTester.java
@@ -0,0 +1,21 @@
+public class Unit1ExercisesTester {
+    public static void main(String[] args) {
+        System.out.println(Unit1Exercises.helloName("Bob"));
+
+        System.out.println(Unit1Exercises.makeOutWord("<<>>", "hello"));
+
+        System.out.println(Unit1Exercises.withoutEnd("hii"));
+
+        System.out.println(Unit1Exercises.comboString("hi", "hello"));
+
+        System.out.println(Unit1Exercises.left2("hello"));
+
+
+
+        System.out.println(Unit1Exercises.withoutEnd2("Hello"));
+
+        System.out.println(Unit1Exercises.stringEnds("Chocolate", 2));
+        
+        System.out.println(Unit1Exercises.hasBad("badxxx"));
+    }
+}
