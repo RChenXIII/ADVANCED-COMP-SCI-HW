73e842f610b3020f9143f3f376189f01a74d3e28
RChenXIII <ryanchen2231@gmail.com>
2026-01-18 14:54:47 -0800

    Finished Unit 4 Exercises

---
+++ Unit Lessons/Unit 4/Labs/Unit 4 Exercises/Unit4Exercises.java
@@ -0,0 +1,139 @@
+import java.util.ArrayList;
+
+public class Unit4Exercises {
+
+    public static boolean matchingEndSequences(int[] nums, int n) {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != nums[nums.length - n + i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean hasThreeConsecutive(int[] nums) {
+        if (nums.length < 3) {
+            return false;
+        }
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            int mod0 = nums[i] % 2;
+            int mod1 = nums[i + 1] % 2;
+            int mod2 = nums[i + 2] % 2;
+            
+            if (mod0 == mod1 && mod1 == mod2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[] generateNumberSequence(int start, int end) {
+        int size = end - start;
+        int[] result = new int[size];
+        
+        for (int i = 0; i < size; i++) {
+            result[i] = start + i;
+        }
+        
+        return result;
+    }
+
+    public static String[] fizzBuzz(int start, int end) {
+        int size = end - start;
+        String[] result = new String[size];
+        
+        for (int i = 0; i < size; i++) {
+            int num = start + i;
+            
+            if (num % 3 == 0 && num % 5 == 0) {
+                result[i] = "FizzBuzz";
+            } else if (num % 3 == 0) {
+                result[i] = "Fizz";
+            } else if (num % 5 == 0) {
+                result[i] = "Buzz";
+            } else {
+                result[i] = String.valueOf(num);
+            }
+        }
+        
+        return result;
+    }
+
+    public static int[] moveEvenBeforeOdd(int[] nums) {
+        int[] result = new int[nums.length];
+        int index = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                result[index] = nums[i];
+                index++;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                result[index] = nums[i];
+                index++;
+            }
+        }
+        
+        return result;
+    }
+
+    public static ArrayList<Integer> noNegatives(ArrayList<Integer> nums) {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) >= 0) {
+                result.add(nums.get(i));
+            }
+        }
+        
+        return result;
+    }
+
+    public static ArrayList<Integer> excludeTeenNumbers(ArrayList<Integer> nums) {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums.get(i);
+            if (num < 13 || num > 19) {
+                result.add(num);
+            }
+        }
+        
+        return result;
+    }
+
+    public static ArrayList<String> appendY(ArrayList<String> strs) {
+        ArrayList<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < strs.size(); i++) {
+            String str = strs.get(i);
+            String withY = str + "y";
+            
+            if (!withY.contains("yy")) {
+                result.add(withY);
+            }
+        }
+        
+        return result;
+    }
+
+    public static ArrayList<Integer> squarePlus10(ArrayList<Integer> nums) {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums.get(i);
+            int squared = num * num + 10;
+            int lastDigit = squared % 10;
+            
+            if (lastDigit != 5 && lastDigit != 6) {
+                result.add(squared);
+            }
+        }
+        
+        return result;
+    }
+}
---
+++ Unit Lessons/Unit 4/Labs/Unit 4 Exercises/Unit4Tester.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Unit4Tester {
+    
+    public static void main(String[] args) {
+        System.out.println("=== Testing Array Methods ===\n");
+        testMatchingEndSequences();
+        testHasThreeConsecutive();
+        testGenerateNumberSequence();
+        testFizzBuzz();
+        testMoveEvenBeforeOdd();
+        
+        System.out.println("\n=== Testing ArrayList Methods ===\n");
+        testNoNegatives();
+        testExcludeTeenNumbers();
+        testAppendY();
+        testSquarePlus10();
+    }
+    
+    public static void testMatchingEndSequences() {
+        System.out.println("Testing matchingEndSequences:");
+        
+        int[] arr = {5, 6, 45, 99, 13, 5, 6};
+        System.out.println("Expected false: " + Unit4Exercises.matchingEndSequences(arr, 1));
+        System.out.println("Expected true: " + Unit4Exercises.matchingEndSequences(arr, 2));
+        System.out.println("Expected false: " + Unit4Exercises.matchingEndSequences(arr, 3));
+        System.out.println();
+    }
+    
+    public static void testHasThreeConsecutive() {
+        System.out.println("Testing hasThreeConsecutive:");
+        
+        System.out.println("Expected true: " + Unit4Exercises.hasThreeConsecutive(new int[]{2, 1, 3, 5}));
+        System.out.println("Expected false: " + Unit4Exercises.hasThreeConsecutive(new int[]{2, 1, 2, 5}));
+        System.out.println("Expected true: " + Unit4Exercises.hasThreeConsecutive(new int[]{2, 4, 2, 5}));
+        System.out.println();
+    }
+    
+    public static void testGenerateNumberSequence() {
+        System.out.println("Testing generateNumberSequence:");
+        
+        System.out.println("Expected [5, 6, 7, 8, 9]: " + Arrays.toString(Unit4Exercises.generateNumberSequence(5, 10)));
+        System.out.println("Expected [11, 12, 13, 14, 15, 16, 17]: " + Arrays.toString(Unit4Exercises.generateNumberSequence(11, 18)));
+        System.out.println("Expected [1, 2]: " + Arrays.toString(Unit4Exercises.generateNumberSequence(1, 3)));
+        System.out.println();
+    }
+    
+    public static void testFizzBuzz() {
+        System.out.println("Testing fizzBuzz:");
+        
+        System.out.println("Expected [1, 2, Fizz, 4, Buzz]: " + Arrays.toString(Unit4Exercises.fizzBuzz(1, 6)));
+        System.out.println("Expected [1, 2, Fizz, 4, Buzz, Fizz, 7]: " + Arrays.toString(Unit4Exercises.fizzBuzz(1, 8)));
+        System.out.println("Expected [1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz]: " + Arrays.toString(Unit4Exercises.fizzBuzz(1, 11)));
+        System.out.println();
+    }
+    
+    public static void testMoveEvenBeforeOdd() {
+        System.out.println("Testing moveEvenBeforeOdd:");
+        
+        System.out.println("Expected [0, 0, 0, 1, 1, 1, 1]: " + Arrays.toString(Unit4Exercises.moveEvenBeforeOdd(new int[]{1, 0, 1, 0, 0, 1, 1})));
+        System.out.println("Expected [2, 3, 3]: " + Arrays.toString(Unit4Exercises.moveEvenBeforeOdd(new int[]{3, 3, 2})));
+        System.out.println("Expected [2, 2, 2]: " + Arrays.toString(Unit4Exercises.moveEvenBeforeOdd(new int[]{2, 2, 2})));
+        System.out.println();
+    }
+    
+    public static void testNoNegatives() {
+        System.out.println("Testing noNegatives:");
+        
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, -2));
+        System.out.println("Expected [1]: " + Unit4Exercises.noNegatives(list1));
+        
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(-3, -3, 3, 3));
+        System.out.println("Expected [3, 3]: " + Unit4Exercises.noNegatives(list2));
+        
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(-1, -1, -1));
+        System.out.println("Expected []: " + Unit4Exercises.noNegatives(list3));
+        System.out.println();
+    }
+    
+    public static void testExcludeTeenNumbers() {
+        System.out.println("Testing excludeTeenNumbers:");
+        
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(12, 13, 19, 20));
+        System.out.println("Expected [12, 20]: " + Unit4Exercises.excludeTeenNumbers(list1));
+        
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 14, 1));
+        System.out.println("Expected [1, 1]: " + Unit4Exercises.excludeTeenNumbers(list2));
+        
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(15));
+        System.out.println("Expected []: " + Unit4Exercises.excludeTeenNumbers(list3));
+        System.out.println();
+    }
+    
+    public static void testAppendY() {
+        System.out.println("Testing appendY:");
+        
+        ArrayList<String> list1 = new ArrayList<>(Arrays.asList("a", "b", "c"));
+        System.out.println("Expected [ay, by, cy]: " + Unit4Exercises.appendY(list1));
+        
+        ArrayList<String> list2 = new ArrayList<>(Arrays.asList("a", "b", "cy"));
+        System.out.println("Expected [ay, by]: " + Unit4Exercises.appendY(list2));
+        
+        ArrayList<String> list3 = new ArrayList<>(Arrays.asList("xx", "ya", "zz"));
+        System.out.println("Expected [xxy, yay, zzy]: " + Unit4Exercises.appendY(list3));
+        System.out.println();
+    }
+    
+    public static void testSquarePlus10() {
+        System.out.println("Testing squarePlus10:");
+        
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(3, 1, 4));
+        System.out.println("Expected [19, 11]: " + Unit4Exercises.squarePlus10(list1));
+        
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1));
+        System.out.println("Expected [11]: " + Unit4Exercises.squarePlus10(list2));
+        
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(2));
+        System.out.println("Expected [14]: " + Unit4Exercises.squarePlus10(list3));
+        System.out.println();
+    }
+}
