f5c375e9191a4baca01bab5306b15306011c05b8
RChenXIII <ryanchen2231@gmail.com>
2026-02-26 14:24:42 -0800

    finished shapes lab

--- Unit Lessons/Unit 5/Labs/Shapes/Circle.java
+++ Unit Lessons/Unit 5/Labs/Shapes/Circle.java
@@ -1,31 +1,44 @@
-// //import java.awt.BasicStroke;
-// //import java.awt.Color;
-// //import java.awt.Graphics;
-// //import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-// public class Circle extends Shape {
+public class Circle extends Shape {
 
-// public Circle(int xCoord, int yCoord, int radius) {
+    private int radius;
 
-// }
+    // three-parameter constructor
+    public Circle(int xCoord, int yCoord, int radius) {
+        super(xCoord, yCoord);
+        this.radius = radius;
+    }
 
-// public Circle(int radius) {
+    // one-parameter constructor (defaults coords to 0)
+    public Circle(int radius) {
+        this(0, 0, radius);
+    }
 
-// }
+    // calculate area of circle
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
 
-// public void draw(Graphics g) {
-// // Initialize a Graphics 2D object
-// // Set the color of the circle
-// // Draw the circle using the fillOval method
+    // draw the circle with fill and outline
+    public void draw(Graphics g) {
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setPaint(Color.blue);
+        g2D.fillOval(xCoord, yCoord, radius, radius);
 
-// // set the color of the circle
-// // change the stroke width
-// // draw the circle using the drawOval method
+        g2D.setPaint(Color.orange);
+        g2D.setStroke(new BasicStroke(5));
+        g2D.drawOval(xCoord, yCoord, radius, radius);
+    }
 
-// }
+    public String toString() {
+        // call parent toString and append circle details
+        double area = getArea();
+        return super.toString() + "\nIt is a circle with a radius of " + radius + ". It's area is "
+                + String.format("%.2f", area) + ".";
+    }
 
-// public String toString() {
-// return "";
-// }
-
-// }
+}
--- Unit Lessons/Unit 5/Labs/Shapes/MyPanel.java
+++ Unit Lessons/Unit 5/Labs/Shapes/MyPanel.java
@@ -11,16 +11,12 @@ public class MyPanel extends JPanel {
     }
 
     public void paint(Graphics g) {
-        // to-do: add a circle to the ArrayList
-        // to-do: draw the circle
-        // to-do: uncomment the code below and run the program
-       
         // Adds shapes to an ArrayList, converting them all to Polygons
         ArrayList<Shape> polygons = new ArrayList<Shape>();
         polygons.add(new Rectangle(125, 125, 250, 250));
-        // polygons.add(new Circle(125, 125, 250));
-        // polygons.add(new Rectangle(175, 175, 150, 150));
-        // polygons.add(new Circle(175, 175, 150));
+        polygons.add(new Circle(125, 125, 250));
+        polygons.add(new Rectangle(175, 175, 150, 150));
+        polygons.add(new Circle(175, 175, 150));
 
 
         // Draws all the shapes in the ArrayList using that shape's draw method
